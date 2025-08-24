644e4f81b8753637b6e7c32a500c0123df83746a
abhiyogesh <110718689+abhiyogesh@users.noreply.github.com>
2025-08-24 14:09:28 +0530

    15-mails

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,7 @@ const transporter = nodemailer.createTransport(
   sendgridTransport({
     auth: {
       api_key:
-        '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'
+        'YOUR_SENDGRID_API_KEY'
     }
   })
 );
